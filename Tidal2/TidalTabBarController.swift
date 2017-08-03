1a8e16cf140a13658b2e248aeefffdf83cb1c458
Ameenah Burhan <ameenahburhan@Ameenahs-Mac-mini.local>
2017-08-03 19:42:07 -0400

    added tab controller

---
+++ Tidal2/TidalTabBarController.swift
@@ -0,0 +1,21 @@
+///Users/ameenahburhan/Desktop/Tidal2/Tidal2/TidalTabBarController.swift
+//  TidalTabBarController.swift
+//  Tidal2
+//
+//  Created by Ameenah Burhan on 7/31/17.
+//  Copyright © 2017 Ameenah Burhan. All rights reserved.
+//
+
+import UIKit
+
+class TidalTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        UITabBar.appearance().barTintColor = UIColor(red: 0.25, green: 0.25, blue: 0.25, alpha: 1)
+
+        
+    }
+
+
+}
