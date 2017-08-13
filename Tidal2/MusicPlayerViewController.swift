b9cdd6d3b4cd3ed8727a59ded9f7c7fcf6174ff2
Ameenah Burhan <ameenahburhan@Ameenahs-Mac-mini.local>
2017-08-13 18:52:53 -0400

    Added music player view controller

---
+++ Tidal2/MusicPlayerViewController.swift
@@ -0,0 +1,22 @@
+//
+//  MusicPlayerViewController.swift
+//  Tidal2
+//
+//  Created by Ameenah Burhan on 8/13/17.
+//  Copyright © 2017 Ameenah Burhan. All rights reserved.
+//
+
+import UIKit
+
+class MusicPlayerViewController: UIViewController {
+
+    @IBOutlet weak var albumImage: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+}
